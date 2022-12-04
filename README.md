64ee78905d227013a2395bfe4fd38a604d318548
Giuseppe Cusati <gisippo.94@gmail.com>
2022-12-04 17:04:40 +0100

    Add comments and miss-spelling check

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Portal-To-MacAuthentication
-Create a MAC Entry to a Windows Server NPS starting from AD User authentication
+Create a MAC Address AD User to Windows Active Directory starting from an AD User. This user will be used in NPS Server to execute MAC Authentication.
+
+# Use Cases:
+## TP-Link Omada
+TP-Link Omada System has the limitation to not permit VLAN Assignment if portal login used. This script, combined with a Django Web Server, can resolve this issue: the Django web server can be set as external portal so the user will login with its AD Credential, the script will check if they are valid and then will create a MAC Address AD User to permit (instead of the User/Password Authentication) the MAC Based Authentication that supports VLAN Assignment.
+
+# Usefull Links:
+[How to setup NPS for Radius Authentication] (https://documentation.meraki.com/MS/Access_Control/Configuring_Microsoft_NPS_for_MAC-Based_RADIUS_-_MS_Switches)
+[How to setup VLAN Assignment] https://www.expertnetworkconsultant.com/configuring/ieee-802-1x-authentication-and-dynamic-vlan-assignment-with-nps-radius-server/
--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import win32evtlog # requires pywin32 pre-installed
 import subprocess
 from datetime import datetime, timedelta, date
 
-server = 'localhost' # name of the target computer to get event logs
+server = 'localhost' # Name of the Server where read the logs from (The NPS Server)
 logtype = 'Security'
 hand = win32evtlog.OpenEventLog(server,logtype)
 flags = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
 total = win32evtlog.GetNumberOfEventLogRecords(hand)
 
 groupSuffix = "" # Suffix/Prefix common for all AD Groups used in Radius Authentication
-ou = "" # OU where to create MacAddress Users ex "OU=MacRadiusUsers,OU=ServiceUsers,DC=local,DC=com"
-accountExpire = 7 # days
+ou = "" # OU where to create MAC Address AD Users ex "OU=MacRadiusUsers,OU=ServiceUsers,DC=local,DC=com"
+accountExpire = 7 # Set expiration of the MAC Address AD User from today in days
 domain = "" # Domain name ex "local.com"
 
 def getLastFailedRadiusLogon():
+    """
+    getLastFailedRadiusLogon Get last unauthorized user event in Windows Event Logs
+
+    :return: MAC Address of the device
+    """ 
+
     i = True
     temp = []
     while i:
@@ -29,6 +35,14 @@ def getLastFailedRadiusLogon():
     return temp[1]
 
 def checkADCredentials(username, password):
+    """
+    checkADCredentials Check if the given credentials are valid
+
+    :param username: Ad Username
+    :param password: Ad Password
+    :return: True or False
+    """ 
+
     cmd = '$CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName \
     $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,"%s","%s") \
     if ($domain.name -eq $null) { \
@@ -41,15 +55,32 @@ def checkADCredentials(username, password):
         return False
 
 def createADUser(username, macaddress):
+    """
+    createADUser Create ad AD User which has Username and Password equal to MAC Address, GivenName as the original Username of the AD User
+    and expires after the day set globally by accountExpire variable
+
+    :param username: Username of the User
+    :param macaddress: MAC Address of the device
+
+    :return: completed message
+    """ 
+
     s = date.today()
     expireDate = s + timedelta(days=accountExpire)
     expireDate = expireDate.strftime("%m/%d/%Y")
 
     cmd = 'New-ADUser -Name %s -Accountpassword (ConvertTo-SecureString -AsPlainText %s -Force) -UserPrincipalName "%s@%s" -SamAccountName %s -GivenName %s -Surname "MAC Address" -path "%s" -Enabled $true -AccountExpirationDate %s -CannotChangePassword $True' % (macaddress, macaddress, macaddress, macaddress, domain, username, ou, expireDate) #Create AD User with MAC Address as Username and Password
     completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
-    print(completed)
+    return completed
 
 def getUserRadiusGroup(username):
+    """
+    getUserRadiusGroup Check original User group membership to extract all AD group related to RadiusAuth (filter by the suffix/prefix set globally by groupSuffix variable)
+
+    :param username: Username of the User
+    :return: RadiusGroup list
+    """ 
+
     cmd = 'Get-ADPrincipalGroupMembership %s | select name' % (username)
     completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
     completed = str(completed).split("\\r\\n")
@@ -58,6 +89,14 @@ def getUserRadiusGroup(username):
     return radiusGroups
 
 def addUserToGroup(macaddress, username):
+    """
+    addUserToGroup Add MAC Address AD User to Radius Authentication AD group
+
+    :param macaddress: MAC Address of the device
+    :param username: Username of the User
+    :return: completed message
+    """ 
+
     radiusGroups = getUserRadiusGroup(username)
     for group in radiusGroups:
         cmd = 'Add-ADGroupMember -Identity "%s" -Members %s' % (group, macaddress)
@@ -67,7 +106,7 @@ def addUserToGroup(macaddress, username):
 username = "" # AD Username
 password = "" # AD Password
 
-if checkADCredentials(username, password):
-    macaddress = getLastFailedRadiusLogon()
-    createADUser(username, macaddress)
-    addUserToGroup(macaddress, username)
+if checkADCredentials(username, password): # Check if credentials are valid
+    macaddress = getLastFailedRadiusLogon() # Get MAC Address of the device to authenticate
+    createADUser(username, macaddress) # Create the MAC Address AD User
+    addUserToGroup(macaddress, username) # Add MAC Address AD User to Radius Authentication Groups
